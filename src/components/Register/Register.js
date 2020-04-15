fe3fb4e12882b4439cfb4d980397f731e22ba811
vinuprasadom <49249104+vinuprasadom@users.noreply.github.com>
2020-04-15 18:17:31 +0530

    Latest changes

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
    }
 
    onRegister = () => {
-      fetch('http://localhost:3000/register', {
+      fetch('https://evening-ravine-34011.herokuapp.com/register', {
          method: 'post',
          headers: {'Content-Type': 'application/json'},
          body: JSON.stringify({
